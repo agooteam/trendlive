021689e371f15889431bea2eff503d5af6f413ca
mswindu <windu.whr@gmail.com>
2015-05-30 22:14:10 +0600

    Задача 5. Добавил отображение страницы с подборками видео

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,20 +1,18 @@
 <?php
 
-
 /********************** GET запросы ****************************************************/
 Route::get('/', 'IndexController@index');//Главная страница
-Route::get('/registration', 'RegistrationController@index');//Страница регистрации
+
 Route::get('/activation/{hash}','RegistrationController@activation'); //Активация акканута
+Route::get('/catalog', 'CollectionController@index');//Страница коллекций
 Route::get('/profile/','ProfileController@index');//Стартовая страница профиля
+Route::get('/profile/change_password','ProfileController@get_change_password');//Изменение пароля
 Route::get('/profile/login','ProfileController@get_login');//Страница авторизации пользователя
 Route::get('/recovery_password','ProfileController@get_recovery_password');//Восстановление пароля
-Route::get('/profile/change_password','ProfileController@get_change_password');//Изменение пароля
-/****************************************************************************************/
+Route::get('/registration', 'RegistrationController@index');//Страница регистрации
 
 /********************** POST запросы ****************************************************/
-Route::post('/registration', 'RegistrationController@registration_user');//Регистриция пользователя
-Route::post('/recovery_password','ProfileController@post_recovery_password');//Восстановление пароля
-Route::post('/profile/change_password','ProfileController@post_change_password');//Изменение пароля
 Route::post('/deploy','WHController@pull');//github Webhooks
-/****************************************************************************************/
-// TEST END COMMENT
+Route::post('/recovery_password','ProfileController@post_recovery_password');//Восстановление пароля
+Route::post('/registration', 'RegistrationController@registration_user');//Регистриция пользователя
+Route::post('/profile/change_password','ProfileController@post_change_password');//Изменение пароля
---
+++ resources/views/Collection/footer.blade.php
@@ -0,0 +1,31 @@
+<div class="footer">
+    <div class="footer_content">
+        <div class="footer_block1">
+            <div class="footer_header">Контакты</div>
+            <div class="footer_txt">Администратор:<br>admin<font color="57c5a0">@trendlive.ru</font><br><br>
+                Техническая поддержка:<br>support<font color="57c5a0">@trendlive.ru</font></div>
+            <div class="footer_header">Мы в социальных сетях</div>
+            <div class="fb"></div>
+            <div class="tw"></div>
+            <div class="vk"></div>
+        </div>
+        <div class="footer_block2">
+            <div class="footer_header">Навигация</div>
+            <div class="footer_navi" onclick="location.href='/';">Главная</div>
+            <div class="footer_navi" onclick="location.href='/catalog';">Подборки видео</div>
+            <div class="footer_navi" onclick="location.href='/profile';">Профиль</div>
+
+
+            <div class="footer_header">TrendLive.ru | © 2015</div>
+        </div>
+        <div class="footer_block2">
+            <div class="footer_header">О нас</div>
+            <div class="footer_txt">TrendLive - это инструмент для создания подборок. Мы работаем для вас, создавайте и делитесь самым интересными видео с друзьями.</div>
+        </div>
+    </div>
+</div>
+
+
+
+</body>
+</html>
