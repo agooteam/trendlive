79272b22b6b795e7e38fd6dc04394d2cf93a4e6d
Mikhail Nilov <mikhail.nilov.nstu@gmail.com>
2015-06-02 18:53:28 +0300

    d

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,7 +10,7 @@ class AuthController extends Controller {
     public function PostOnWallVk(Request $request){
         $code = $request->get('code');//Получаем код
         $collection_id = $request->get('state');//ID коллекции
-
+        return $request->all();
         $vk = \OAuth::consumer('Vkontakte');
         if ( !is_null($code)){
             $token = $vk->requestAccessToken($code); // получаем токен
