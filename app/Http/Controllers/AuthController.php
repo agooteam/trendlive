a1ab30065b4bfa2f9d9143b4b4566afeacc12210
Mikhail Nilov <mikhail.nilov.nstu@gmail.com>
2015-06-02 19:22:00 +0300

    d

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,8 +1,8 @@
 <?php namespace TrendLive\Http\Controllers;
 
+use Artdarek\OAuth\Facade\OAuth;
 use TrendLive\Http\Requests;
 use TrendLive\Http\Controllers\Controller;
-
 use Illuminate\Http\Request;
 
 class AuthController extends Controller {
@@ -14,7 +14,8 @@ class AuthController extends Controller {
         $vk = \OAuth::consumer('Vkontakte');
         if ( !is_null($code)){
             $token = $vk->requestAccessToken($code); // получаем токен
-            return $token->extraParams['user_id'];
+            $param = $token-> getExtraParams();
+            return dd($param);
             $result = json_decode($vk->request('/me'), true);
             $message = 'Your unique facebook user id is: ' . $result['id'] . ' and your name is ' . $result['name'];
             return $message. "<br/>";
