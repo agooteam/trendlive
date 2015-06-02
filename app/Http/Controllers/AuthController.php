62ca34f607edd9794882cbf3108af556ec88c1d9
Mikhail Nilov <mikhail.nilov.nstu@gmail.com>
2015-06-02 21:12:30 +0300

    d

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,16 +4,15 @@ use Artdarek\OAuth\Facade\OAuth;
 use TrendLive\Http\Requests;
 use TrendLive\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 
 class AuthController extends Controller {
 
     public function PostOnWallVk(Request $request){
-        $code = $request->get('code');//Получаем код
-        return $request-> get('access_token');
-        $collection_id = $request->get('state');//ID коллекции
-        $request->all();
-        $vk = \OAuth::consumer('Vkontakte');
-        if ( !is_null($code)){
+        $request-> get('access_token');
+        $url =  URL::full();
+        return $url;
+        /*if ( !is_null($code)){
             $token = $vk->requestAccessToken($code); // получаем ответ
             $access_token = $token->getAccessToken();//токен access
             $param = $token-> getExtraParams();//получаем параметры ответа
@@ -28,7 +27,7 @@ class AuthController extends Controller {
         else{
             $url = $vk->getAuthorizationUri();
             return redirect((string)$url);
-        }
+        }*/
     }
 
 }
