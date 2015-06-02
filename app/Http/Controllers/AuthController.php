ba76925b7f8bb7cac902a8f81ccc8e6dfa83b852
Mikhail Nilov <mikhail.nilov.nstu@gmail.com>
2015-06-02 18:50:54 +0300

    work

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php namespace TrendLive\Http\Controllers;
+
+use TrendLive\Http\Requests;
+use TrendLive\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller {
+
+    public function PostOnWallVk(Request $request){
+        $code = $request->get('code');//Получаем код
+        $collection_id = $request->get('state');//ID коллекции
+
+        $vk = \OAuth::consumer('Vkontakte');
+        if ( !is_null($code)){
+            $token = $vk->requestAccessToken($code); // получаем токен
+            $result = json_decode($vk->request('/me'), true);
+            $message = 'Your unique facebook user id is: ' . $result['id'] . ' and your name is ' . $result['name'];
+            return $message. "<br/>";
+        }
+        else{
+            $url = $vk->getAuthorizationUri();
+            return redirect((string)$url);
+        }
+    }
+
+}
