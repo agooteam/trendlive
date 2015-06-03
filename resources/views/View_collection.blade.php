4d3a98fa0dc3794c031ea27d41086e5d0e27a383
Mikhail Nilov <mikhail.nilov.nstu@gmail.com>
2015-06-03 06:36:25 +0300

    d

--- resources/views/View_collection.blade.php
+++ resources/views/View_collection.blade.php
@@ -26,7 +26,6 @@
         <div class="l-name">Всего видео:</div>
         <div class="number">{{$collection->count_videos}}</div>
         @if($collection -> image_preview_url == null)
-        <div class="img"><img src="/assets/collections/preview/default_collection.png"/></div>
         <div class="number"><br><a href="http://vk.com/share.php?url={{$vk['CURRENT_URL']}}/collection/view/{{$collection-> id}}&title={{$collection-> collection_name}}&description={{$collection->description}}&image={{$vk['CURRENT_URL']}}/assets/collections/preview/default_collection.png">Поделиться</a></div>
         @else
         <div class="number"><br><a href="http://vk.com/share.php?url={{$vk['CURRENT_URL']}}/collection/view/{{$collection-> id}}&title={{$collection-> collection_name}}&description={{$collection->description}}&image={{$collection -> image_preview_url}}">Поделиться</a></div>
